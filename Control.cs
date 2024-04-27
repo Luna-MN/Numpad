748859ba0082f8ac27ea52dd3deb561fcfe3c994
Matthew Nock <thematt.nock@gmail.com>
2024-04-27 12:44:42 +0100

    on key press
    displays when KP

--- Control.cs
+++ Control.cs
@@ -6,8 +6,6 @@ public partial class Control : MeshInstance2D
 {
 	[Export]
 	public Key pressedKey;
-	[Export]
-	public MeshInstance2D obbi;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -16,15 +14,17 @@ public partial class Control : MeshInstance2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(Input.IsKeyPressed(pressedKey)){
+			this.Visible = true;
+		}
+		if(!Input.IsKeyPressed(pressedKey)){
+			this.Visible = false;
+		}
 	}
 	public void onKeyPress()
 	{
-			if(Input.IsKeyPressed(pressedKey)){
-				obbi.Visible = true;
-			}
-			else{
-				obbi.Visible = false;
-			}
+
+
 	}
 	public override void _Input(InputEvent @event){
 		if (@event is InputEventKey keyEvent && keyEvent.Pressed){
