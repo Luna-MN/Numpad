0a4e0e2c6e256d2d5b6a804295a9c784c9e611fd
Matthew Nock <thematt.nock@gmail.com>
2024-04-29 22:42:53 +0100

    Random :)

--- Random.cs
+++ Random.cs
@@ -1,23 +1,35 @@
 using Godot;
 using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 
 public partial class Random : Node
 {
 	List<int> keys = new List<int>();
+	private int key;
+	private bool Generated = false;
+	[Export]
+    public ColorRect[] KeyRects;
+	public void RandomizeRects(){
+		int ammount = GD.RandRange(1, 4);
+		for (int i = 0; i < ammount; i++) {
+			key = GD.RandRange(0, 10);
+		}
+	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		int ammount = GD.RandRange(0, 4);
-		for (int i = 0; i < ammount; i++)
-		{
-			int key = GD.RandRange(0, 11);
-			keys.Add(key);
+	public override void _Process(double delta) {
+		foreach (int key in keys) {
+			KeyRects[key].Visible = true;
 		}
+		if(Generated == true){
+			RandomizeRects();
+			Generated = false;
+		}		
 	}
 }
